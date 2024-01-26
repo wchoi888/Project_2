a76fdb95ed1e6a649650f444d13ebab9844efc6a
Wing Choi <wchoi888@live.com>
2024-01-26 15:15:56 -0500

    updated server.js with more config changes for heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
-
+app.set("trust proxy", 1); // Enable trust proxy to read X-Forwarded-Proto header
 const sess = {
   secret: process.env.SESSION_SECRET || "SuperSecretSessionSecret", // Use a secure secret
   cookie: {
